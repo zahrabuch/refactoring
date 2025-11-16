083ea06e27f272b528bea0711f550616842b1e32
zahrabuch <zahra.buchanan@mail.utoronto.ca>
2025-11-16 18:49:41 -0500

    Task 1.1

--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,12 +1,23 @@
 package theater;
 
+/**
+ * Class representing a play.
+ */
 public class Play {
 
-    public String name;
-    public String type;
+    private String name;
+    private String type;
 
     public Play(String name, String type) {
         this.name = name;
         this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
     }
 }
